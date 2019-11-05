d7bcfb35569ec5f9f61d0e48fd760f66aa1916ba
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-11-05 00:14:04 -0300

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,23 @@
-CREATE TABLE notes (id integer primary key, content text, datetime text);
-CREATE TABLE tags (id integer primary key, tag text not null);
-CREATE TABLE notes_tags (notes_id integer not null, tags_id integer not null, foreign key (notes_id) references notes (id) on delete cascade, foreign key (tags_id) references tags (id) on delete cascade, primary key (notes_id, tags_id));
+CREATE TABLE
+    notes (
+        id integer primary key,
+        content text,
+        datetime text
+    );
+CREATE TABLE
+    tags (
+        id integer primary key,
+        tag text not null
+    );
+CREATE TABLE 
+    notes_tags (
+        id integer primary key,
+        notes_id integer not null,
+        tags_id integer not null,
+        foreign key 
+            (notes_id) 
+            references notes (id),
+        foreign key 
+            (tags_id)
+            references tags (id)
+    );
