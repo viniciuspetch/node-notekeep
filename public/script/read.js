c9892dea91fa90ff2e1c5b78d356fe68b56d1c38
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-11-04 23:47:38 -0300

    Fixed wrong id

--- public/script/read.js
+++ public/script/read.js
@@ -8,7 +8,7 @@ $(function() {
         for (let i = 0; i < data.length; i++) {
             console.log(data[i]);
             $('#noteList').append('<p>' + data[i].id + ' ['+data[i].tags+']:<br>' + data[i].content+'<br>' + data[i].datetime + 
-            '<br> <a href="/edit/' + i + '">Edit</a>' + '</p>');
+            '<br> <a href="/edit/' + data[i].id + '">Edit</a>' + '</p>');
         }
     });
 });
