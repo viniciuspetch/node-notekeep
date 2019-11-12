a64ee121fca078f5c45641b6707dfde6254c79fc
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-11-12 13:45:06 -0300

    Added a user table, timestamps

--- schema.sql
+++ schema.sql
@@ -1,8 +1,22 @@
+CREATE TABLE 
+    user_acc (
+        id integer primary key,
+        usrn text not null,
+        pswd text not null,
+        creation integer not null,
+        lastupdated integer not null,
+    );
+
 CREATE TABLE
     notes (
         id integer primary key,
-        content text,
-        datetime text
+        user_id integer not null,
+        content text not null,
+        creation integer not null,
+        lastupdated integer not null,
+        foreign key
+            (user_id)
+            references user_acc (id)
     );
 
 CREATE TABLE
@@ -25,11 +39,16 @@ CREATE TABLE
     );
 
 INSERT INTO 
-    notes(content, datetime)
+    user_acc(usrn, pswd, creation, lastupdated)
 VALUES
-    ('note 1', 123),
-    ('note 2', 456),
-    ('note 3', 789);
+    ('default', 'default', 1000, 1500);
+
+INSERT INTO 
+    notes(user_id, content, creation, lastupdated)
+VALUES
+    (1, 'note 1', 1000, 1500),
+    (1, 'note 2', 1000, 2000),
+    (1, 'note 3', 1750, 2250);
 
 INSERT INTO 
     tags(tag)
