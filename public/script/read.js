de8f321ab00d26e2a3a8caf9404abec6c33c9861
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-11-14 01:09:38 -0300

    Organizing read.js

--- public/script/read.js
+++ public/script/read.js
@@ -8,10 +8,19 @@ $(function() {
         console.log(response);
         for (let i = 0; i < response.length; i++) {
             console.log(response[i]);
-            $('#noteList').append('<p>' + response[i].id +
-            ' ['+response[i].tags+']:<br>' + response[i].content+'<br>' +
-            response[i].datetime + '<br><a href="/edit/' + response[i].id +
-            '">Edit</a><br><a href="/api/delete/' + response[i].id + '">Delete</a></p>');
+            $('#noteList').append('<p>'
+            + response[i].id
+            + ' | ' 
+            + response[i].tags 
+            + '<br>'
+            + response[i].content
+            + '<br>'
+            + response[i].lastupdated
+            + ' | <a href="/edit/'
+            + response[i].id
+            +'">Edit</a> | <a href="/api/delete/'
+            + response[i].id
+            + '">Delete</a></p>');
         }
     });
 });
