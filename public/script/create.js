d975123c97f682a990a9348fe403c031106eeb10
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-11-25 12:25:18 -0300

    Create client-side validation

--- public/script/create.js
+++ public/script/create.js
@@ -1,3 +1,31 @@
+function isAlphaNumeric(str) {
+    for (let i = 0; i < str.length; i++) {
+        let code = str.charCodeAt(i);
+        if (!(code > 47 && code < 58) && // numeric (0-9)
+            !(code > 64 && code < 91) && // upper alpha (A-Z)
+            !(code > 96 && code < 123)) { // lower alpha (a-z)
+            return false;
+        }
+    }
+    return true;
+};
+
+function validateNote(content, tags) {
+    if (!content) {
+        alert('Content is empty');
+        return false;
+    }
+    let tagsList = tags.split(',');
+    for (let i = 0; i < tagsList.length; i++) {
+        if (!isAlphaNumeric(tagsList[i])) {
+            alert('Tags must have letters or numbers only');
+            return false;
+        }
+    }
+
+    return true;
+}
+
 function create() {
     console.log('create()');
 
@@ -6,14 +34,18 @@ function create() {
     let tags = $('#tags').val();
     let token = localStorage.getItem('token');
 
-    $.post(url, {content, tags, token}, (data) => {
+    $.post(url, {
+        content,
+        tags,
+        token
+    }, (data) => {
         console.log('test');
     });
 
     return false;
 }
 
-$(function() {
+$(function () {
     console.log('create.js');
     console.log(localStorage.getItem('token'));
 });
