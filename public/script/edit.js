1b258b3ac2d0aadf440ba5c58b69c4b260ff1c56
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-11-25 13:07:21 -0300

    Edit client-side validation

--- public/script/edit.js
+++ public/script/edit.js
@@ -1,3 +1,31 @@
+function isAlphaNumeric(str) {
+    for (let i = 0; i < str.length; i++) {
+        let code = str.charCodeAt(i);
+        if (!(code > 47 && code < 58) && // numeric (0-9)
+            !(code > 64 && code < 91) && // upper alpha (A-Z)
+            !(code > 96 && code < 123)) { // lower alpha (a-z)
+            return false;
+        }
+    }
+    return true;
+};
+
+function validateNote(content, tags) {
+    if (!content) {
+        alert('Content is empty');
+        return false;
+    }
+    let tagsList = tags.split(',');
+    for (let i = 0; i < tagsList.length; i++) {
+        if (!isAlphaNumeric(tagsList[i])) {
+            alert('Tags must have letters or numbers only');
+            return false;
+        }
+    }
+
+    return true;
+}
+
 function edit() {
     console.log('edit()');
 
@@ -8,13 +36,20 @@ function edit() {
     let currUrlArray = window.location.href.split('/');
     let noteId = currUrlArray[currUrlArray.length - 1];
 
-    let input = {content, tags, token, id: noteId};
+    let input = {
+        content,
+        tags,
+        token,
+        id: noteId
+    };
     console.log(input);
 
-    $.post(url, input, (output) => {
-        console.log(output);
-        console.log('edit post');
-    })
+    if (validateNote(content, tags)) {
+        $.post(url, input, (output) => {
+            console.log(output);
+            console.log('edit post');
+        });
+    };
 
     return false;
 }
