8d6244724760310afad7b0b390976f924e996f99
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-11-25 12:19:51 -0300

    Login client-side validation

--- public/script/login.js
+++ public/script/login.js
@@ -1,22 +1,40 @@
+function validateLogin(username, password) {
+    if (!username) {
+        alert('Username is empty');
+        return false;
+    }
+    if (!password) {
+        alert('Password is empty');
+        return false;
+    }
+
+    return true;
+}
+
 function login() {
     console.log("login()");
-    
+
     let url = "http://localhost:8000/login";
     let username = $('#username').val();
     let password = $('#password').val();
+    if (validateLogin(username, password)) {
+        $.post(url, {
+            username,
+            password
+        }, (data) => {
+            console.log('result: ' + data.result);
+            if (data.reason) console.log('reason: ' + data.reason);
+            if (data.token) console.log('token: ' + data.token);
+            if (data.result == true) {
+                localStorage.setItem('token', data.token);
+                window.location.href = '../';
+            }
+        });
+    }
 
-    $.post(url, {username, password}, (data) => {
-        console.log('result: ' + data.result);
-        if (data.reason) console.log('reason: ' + data.reason);
-        if (data.token) console.log('token: ' + data.token);
-        if (data.result == true) {
-            localStorage.setItem('token', data.token);
-            window.location.href = '../';
-        }
-    });
 }
 
-$(function() {
+$(function () {
     console.log('login.js');
     console.log(localStorage.getItem('token'));
 });
