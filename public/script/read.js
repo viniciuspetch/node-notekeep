43c93798410046f94369f13b3ad974139ace28a9
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-11-26 10:34:21 -0300

    Added update button to refresh note list

--- public/script/read.js
+++ public/script/read.js
@@ -1,26 +1,26 @@
-$(function() {
-    console.log('read.js');
-    console.log(localStorage.getItem('token'));
-    
-    token = localStorage.getItem('token');
-    data = {token};
-    $.post("http://localhost:8000/api/read", data, (response) => {
-        console.log(response);
-        for (let i = 0; i < response.length; i++) {
-            console.log(response[i]);
-            $('#noteList').append('<p>'
-            + response[i].id
-            + ' | ' 
-            + response[i].tags 
-            + '<br>'
-            + response[i].content
-            + '<br>'
-            + response[i].lastupdated
-            + ' | <a href="/edit/'
-            + response[i].id
-            +'">Edit</a> | <a href="/api/delete/'
-            + response[i].id
-            + '">Delete</a></p>');
-        }
+function readPost(data) {
+  $.post("http://localhost:8000/api/read", data, (response) => {
+    console.log(response);
+    for (let i = 0; i < response.length; i++) {
+      console.log(response[i]);
+      $('#noteList').append('<p>' + response[i].id + ' | ' + response[i].tags + '<br>' + response[i].content + '<br>' + response[i].lastupdated + ' | <a href="/edit/' + response[i].id + '">Edit</a> | <a href="/api/delete/' + response[i].id + '">Delete</a></p>');
+    }
+  });
+}
+
+$(function () {
+  console.log('read.js');
+  console.log(localStorage.getItem('token'));
+
+  $('#updateLink').click(function () {
+    $('#noteList').empty();
+    readPost({
+      token
     });
+  });
+
+  token = localStorage.getItem('token');
+  readPost({
+    token
+  });
 });
