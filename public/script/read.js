dcf96871fc8247631d81d603b51783b136e1f01c
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-11-26 21:43:06 -0300

    /api/delete is now POST and checks username and id

--- public/script/read.js
+++ public/script/read.js
@@ -1,10 +1,31 @@
+function apiDelete(token, id) {
+
+  alert('here');
+  body = {
+    token,
+    id
+  };
+  $.post("http://localhost:8000/api/delete", body, (data) => {
+    console.log('Note deleted');
+    return false;
+  });
+}
+
 function readPost(data) {
   $.post("http://localhost:8000/api/read", data, (response) => {
     console.log(response);
     for (let i = 0; i < response.length; i++) {
       console.log(response[i]);
-      $('#noteList').append('<p>' + response[i].id + ' | ' + response[i].tags + '<br>' + response[i].content + '<br>' + response[i].lastupdated + ' | <a href="/edit/' + response[i].id + '">Edit</a> | <a href="/api/delete/' + response[i].id + '">Delete</a></p>');
+      $('#noteList').append('<p>' + response[i].id + ' | ' + response[i].tags + '<br>' + response[i].content + '<br>' + response[i].lastupdated + ' | <a href="/edit/' + response[i].id + '">Edit</a> | <a id="noteItem_' + response[i].id + '" href="#">Delete</a></p>');
+
+      let noteId = '#noteItem_' + response[i].id;
+
+      console.log($(noteId).click(() => {
+        apiDelete(token, response[i].id);
+      }));
     }
+
+    return false;
   });
 }
 
