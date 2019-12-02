b280ba6619bb33481e7b67118ea3738aa9b9f212
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-12-02 09:22:27 -0300

    Tag testing page

---
+++ public/script/tag.js
@@ -0,0 +1,68 @@
+$(function () {
+  // GET
+  $.ajax({
+    url: "/api/tag",
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer " + localStorage.getItem('token'),
+    },
+    data: {},
+  }).done((data) => {
+    console.log('GET /api/tag');
+    console.log(data);
+  });
+
+  // GET id
+  $.ajax({
+    url: "/api/tag/" + 1,
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer " + localStorage.getItem('token'),
+    },
+    data: {},
+  }).done((data) => {
+    console.log('GET /api/tag/:id');
+    console.log(data);
+  });
+
+  // POST
+  $.ajax({
+    url: "/api/tag",
+    method: "POST",
+    headers: {
+      "Authorization": "Bearer " + localStorage.getItem('token'),
+    },
+    data: {
+      tag: 'asd',
+    },
+  }).done((data) => {
+    console.log('POST /api/tag/:id');
+    console.log(data);
+  });
+
+  // PUT
+  $.ajax({
+    url: "/api/tag/" + 1,
+    method: "PUT",
+    headers: {
+      "Authorization": "Bearer " + localStorage.getItem('token'),
+    },
+    data: {},
+  }).done((data) => {
+    console.log('PUT /api/tag/:id');
+    console.log(data);
+  });
+
+  // DELETE
+  $.ajax({
+    url: "/api/tag/" + 1,
+    method: "DELETE",
+    headers: {
+      "Authorization": "Bearer " + localStorage.getItem('token'),
+    },
+    data: {},
+  }).done((data) => {
+    console.log('DELETE /api/tag/:id');
+    console.log(data);
+  });
+})
