71b4c07c642717b6fd2dc552baeb4e650a204271
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-12-17 12:39:20 -0300

    Updated client login

--- public/script/login.js
+++ public/script/login.js
@@ -17,21 +17,25 @@ function login() {
   let url = "http://localhost:8000/api/login";
   let username = $('#username').val();
   let password = $('#password').val();
+
   if (validateLogin(username, password)) {
-    $.post(url, {
-      username,
-      password
-    }, (data) => {
-      console.log('result: ' + data.result);
-      if (data.reason) console.log('reason: ' + data.reason);
-      if (data.token) console.log('token: ' + data.token);
-      if (data.result == true) {
-        localStorage.setItem('token', data.token);
-        window.location.href = '../';
-      }
+    console.log('Status: User information validated');
+
+    $.ajax({
+      url: "/api/login",
+      method: "POST",
+      data: {
+        username,
+        password,
+      },
+    }).done((data, textStatus, xhr) => {
+      console.log('(' + xhr.status + ') ' + textStatus + '/' + xhr.statusText + ': ' + data);
+      localStorage.setItem('token', data.token);
+      window.location.href = '../';
+    }).fail((xhr, textStatus, err) => {
+      console.log('(' + xhr.status + ') ' + textStatus + '/' + err + ': ' + xhr.responseText);
     });
   }
-
 }
 
 $(function () {
