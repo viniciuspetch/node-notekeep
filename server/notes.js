d52d44e6672798b084f8b2a2301fb0caa8cdb5bd
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-12-23 19:34:31 -0300

    Notes now use tags properly

--- server/notes.js
+++ server/notes.js
@@ -43,27 +43,80 @@ exports.getSingle = function (req, res, next) {
 
 exports.post = function (req, res, next) {
   console.log('Middleware: notes.post');
-  if (!res.locals.username) {
+
+  let username = res.locals.username;
+  let content = req.body.content;
+
+  if (!username) {
     res.sendStatus(500);
     return next();
   }
 
-  if (!req.body.content) {
+  if (!content) {
     console.log("Client error: There's no content");
     res.sendStatus(400);
     return next();
   }
 
-  let db = new sqlite3.Database('note.db');
+  let userId = res.locals.user_id;
   let datetime = Date.now();
-  db.run('INSERT INTO notes(user_id, content, creation, lastupdated) VALUES (?,?,?,?)', [res.locals.user_id, req.body.content, datetime, datetime], function (err) {
+  let db = new sqlite3.Database('note.db');
+
+  db.run('INSERT INTO notes(user_id, content, creation, lastupdated) VALUES (?,?,?,?)', [userId, content, datetime, datetime], function (err) {
     if (err) {
       console.log(err);
       res.sendStatus(500);
       return;
     }
-    res.sendStatus(200);
-    return next();
+
+    let tagList = req.body['tags[]'];
+    if (typeof (tagList) == 'string') {
+      tagList = [tagList];
+    }
+    let tagListLength = tagList.length;
+    let noteId = this.lastID;
+
+    for (let i = 0; i < tagListLength; i++) {
+      db.get('SELECT id FROM tags WHERE tag = ? AND user_id = ?', [tagList[i], userId], function (err, row) {
+        if (err) {
+          console.log(err);
+          res.sendStatus(500);
+          return;
+        }
+
+        if (!row) {
+          db.run('INSERT into tags(user_id, tag) VALUES (?, ?)', [userId, tagList[i]], function (err) {
+            if (err) {
+              console.log(err);
+              res.sendStatus(500);
+              return;
+            }
+
+            let tagId = this.lastID;
+            db.run('INSERT into notes_tags(notes_id, tags_id) VALUES (?, ?)', [noteId, tagId], function (err) {
+              if (err) {
+                console.log(err);
+                res.sendStatus(500);
+                return;
+              }
+              res.sendStatus(200);
+              return;
+            });
+          })
+        } else {
+          let tagId = row.id;
+          db.run('INSERT into notes_tags(notes_id, tags_id) VALUES (?, ?)', [noteId, tagId], function (err) {
+            if (err) {
+              console.log(err);
+              res.sendStatus(500);
+              return;
+            }
+            res.sendStatus(200);
+            return;
+          });
+        }
+      });
+    }
   });
 }
 
