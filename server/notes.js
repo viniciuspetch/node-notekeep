859795b76f53100b54bbe8f8da6bee7f3b13e675
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-01-07 12:39:20 -0300

    Fixing empty tag errors, editing now update tags

--- server/notes.js
+++ server/notes.js
@@ -9,7 +9,7 @@ exports.getAll = function (req, res, next) {
   }
 
   let db = new sqlite3.Database('note.db');
-  db.all('SELECT notes.id, notes.content, notes.lastupdated, tags.tag FROM notes LEFT JOIN notes_tags LEFT JOIN tags WHERE notes.user_id = ? AND notes.id = notes_tags.notes_id AND notes_tags.tags_id = tags.id ORDER BY notes.id', [res.locals.user_id], function (err, rows) {
+  db.all('SELECT notes.id, notes.content, notes.lastupdated, tags.tag FROM notes LEFT JOIN notes_tags LEFT JOIN tags WHERE notes.user_id = ? AND notes.id = notes_tags.notes_id AND notes_tags.tags_id = tags.id ORDER BY notes.id, tags.id', [res.locals.user_id], function (err, rows) {
     if (err) {
       console.log(err);
       res.sendStatus(500);
@@ -20,29 +20,33 @@ exports.getAll = function (req, res, next) {
     let newRow = null;
     let newTagList = [];
     let newRowList = [];
+    if (rows.length > 0) {
+      newRow = {
+        id: rows[0].id,
+        content: rows[0].content,
+        lastupdated: rows[0].lastupdated,
+      };
 
-    console.log(typeof (rows));
-    console.log(typeof (newTagList));
-
-    for (let i = 0; i < rows.length; i++) {
-      if (currId != rows[i].id) {
-        if (currId != null) {
+      for (let i = 1; i < rows.length; i++) {
+        // New note, reset variables
+        if (currId != rows[i].id) {
           newRow.tag = newTagList;
           newRowList.push(newRow);
+          newTagList = [];
+          newRow = {
+            id: rows[i].id,
+            content: rows[i].content,
+            lastupdated: rows[i].lastupdated,
+          };
+          currId = rows[i].id;
         }
-        newTagList = [];
-        newRow = {
-          id: rows[i].id,
-          lastupdated: rows[i].lastupdated
-        };
-        currId = rows[i].id;
+        // Keep adding current tag to the tag list
+        newTagList.push(rows[i].tag);
       }
-      newTagList.push(rows[i].tag);
-    }
+      newRow.tag = newTagList;
 
-    newRow.tag = newTagList;
-    newRowList.push(newRow);
-    console.log(newRowList);
+      newRowList.push(newRow);
+    }
 
     res.status(200);
     res.send(newRowList);
@@ -58,14 +62,29 @@ exports.getSingle = function (req, res, next) {
   }
 
   let db = new sqlite3.Database('note.db');
-  db.all('SELECT id, content, lastupdated FROM notes WHERE user_id = ? AND id = ?', [res.locals.user_id, req.params.id], function (err, rows) {
+  db.all('SELECT notes.id, notes.content, notes.lastupdated, tags.tag FROM notes LEFT JOIN notes_tags LEFT JOIN tags WHERE notes.user_id = ? AND notes.id = notes_tags.notes_id AND notes_tags.tags_id = tags.id AND notes.id = ? ORDER BY notes.id, tags.id', [res.locals.user_id, req.params.id], function (err, rows) {
     if (err) {
       console.log(err);
       res.sendStatus(500);
       return;
     }
+
+    let newTagList = []
+    for (let i = 0; i < rows.length; i++) {
+      newTagList.push(rows[i].tag);
+    }
+    let newRow = {
+      id: rows[0].id,
+      content: rows[0].content,
+      lastupdated: rows[0].lastupdated,
+      tag: newTagList,
+    };
+
+
+    console.log(newRow);
+
     res.status(200);
-    res.send(rows);
+    res.send(newRow);
     return next();
   });
 };
@@ -160,14 +179,79 @@ exports.put = function (req, res, next) {
     return next();
   }
 
+  let userId = res.locals.user_id;
+  let noteId = req.params.id;
+
+  console.log('userId: ' + userId);
+  console.log('noteId: ' + noteId);
+
   let db = new sqlite3.Database('note.db');
   let datetime = Date.now();
-  db.run('UPDATE notes SET content = ?, lastupdated = ? WHERE id = ? AND user_id = ?', [req.body.content, datetime, req.params.id, res.locals.user_id], function (err) {
+  db.run('UPDATE notes SET content = ?, lastupdated = ? WHERE id = ? AND user_id = ?', [req.body.content, datetime, noteId, userId], function (err) {
     if (err) {
       console.log(err);
       res.sendStatus(500);
       return;
     }
+
+    if (req.body.tags) {
+      let tagList = req.body.tags;
+      if (typeof (tagList) == 'string') {
+        tagList = [tagList];
+      }
+      let tagListLength = tagList.length;
+
+      for (let i = 0; i < tagListLength; i++) {
+        db.get('SELECT id FROM tags WHERE tag = ? AND user_id = ?', [tagList[i], userId], function (err, row) {
+          console.log('Get tag');
+          if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+          }
+          console.log(noteId);
+          db.run('DELETE FROM notes_tags WHERE notes_id = ?', [noteId], function (err) {
+            console.log('Delete note-tag relationship');
+            if (err) {
+              console.log(err);
+              res.sendStatus(500);
+              return;
+            }
+            /*
+            console.log(row);
+            if (!row) {
+              db.run('INSERT into tags(user_id, tag) VALUES (?, ?)', [userId, tagList[i]], function (err) {
+                if (err) {
+                  console.log(err);
+                  res.sendStatus(500);
+                  return;
+                }
+
+                let tagId = this.lastID;
+                db.run('INSERT into notes_tags(notes_id, tags_id) VALUES (?, ?)', [noteId, tagId], function (err) {
+                  if (err) {
+                    console.log(err);
+                    res.sendStatus(500);
+                    return;
+                  }
+                });
+              })
+            } else {
+              let tagId = row.id;
+              db.run('INSERT into notes_tags(notes_id, tags_id) VALUES (?, ?)', [noteId, tagId], function (err) {
+                if (err) {
+                  console.log(err);
+                  res.sendStatus(500);
+                  return;
+                }
+              });
+            }
+            */
+          });
+        });
+      }
+    }
+
     res.sendStatus(200);
     return next();
   });
