604f6a78ca356076aff7c3887b4ab2aafdaa5eba
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-01-15 13:38:26 -0300

    Initial version of tag filtering, need to fix updating

--- public/script/read.js
+++ public/script/read.js
@@ -1,12 +1,36 @@
 function search() {
+  console.log("Function: search()");
   let searchString = $('#searchbar').val();
+  let tagString = $('#tag-list').val();
   $(".note-item").each(function () {
-    if ($(this).find(".note-content").text().includes(searchString) || $(this).find(".note-tags").text().includes(searchString)) {
-      $(this).show();
+    if ($(this).find(".note-tags").text().includes(tagString)) {
+      if ($(this).find(".note-content").text().includes(searchString) || $(this).find(".note-tags").text().includes(searchString)) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
     } else {
       $(this).hide();
     }
+  });
+}
 
+function tagUsed() {
+  console.log('Function: tagUsed()');
+  $.ajax({
+    url: "/api/tagUsed",
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer " + localStorage.getItem('token'),
+    },
+  }).done((data, textStatus, xhr) => {
+    console.log('(' + xhr.status + ') ' + textStatus + '/' + xhr.statusText + ': ' + data);
+
+    for (let i = 0; i < data.length; i++) {
+      $("#tag-list").append('<option value="' + data[i].tag + '">' + data[i].tag + '</a>');
+    }
+  }).fail((xhr, textStatus, err) => {
+    console.log('(' + xhr.status + ') ' + textStatus + '/' + err + ': ' + xhr.responseText);
   });
 }
 
@@ -79,9 +103,11 @@ $(function () {
   console.log('read.js');
   console.log(localStorage.getItem('token'));
   readPost();
+  tagUsed();
   token = localStorage.getItem('token');
   $('#updateLink').click(function () {
     $('#noteList').empty();
     readPost();
   });
+  $("#tag-list").change(search());
 });
