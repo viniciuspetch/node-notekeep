7e97b8cf0fe3f0c5cdda4a93a2104a928e3f1d44
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-03-11 14:21:34 -0300

    JWT db access adapted for Heroku

--- server/jwt.js
+++ server/jwt.js
@@ -1,72 +1,91 @@
-const jsonwebtoken = require('jsonwebtoken');
-const {
-  Client
-} = require('pg');
+const jsonwebtoken = require("jsonwebtoken");
+const { Client } = require("pg");
 
 // JWT Creation
-exports.newJWT = function (username, secret) {
+exports.newJWT = function(username, secret) {
   if (username == null || secret == null) {
-    console.log('Internal error: no username or secret');
+    console.log("Internal error: no username or secret");
     return false;
   }
-  /*
-  return jsonwebtoken.sign({
-    username: username
-  }, secret, {
-    expiresIn: '24h'
-  });
-  */
- return jsonwebtoken.sign({
-  username: username
-}, secret);
-}
+  return jsonwebtoken.sign(
+    {
+      username: username
+    },
+    secret
+  );
+};
 
 // JWT Verification
-exports.verifyJWT = function (token, secret) {
-  console.log('Function: verifyJWT');
+exports.verifyJWT = function(token, secret) {
+  console.log("Function: verifyJWT");
   if (token == null) {
-    console.log('Request Error: Token is null');
+    console.log("Request Error: Token is null");
     return false;
   }
   if (secret == null) {
-    console.log('Internal Error: Secret is null');
+    console.log("Internal Error: Secret is null");
     return false;
   }
   try {
     return jsonwebtoken.verify(token, secret).username;
   } catch (err) {
-    console.log('Request Error: Json Web Token received is invalid');
+    console.log("Request Error: Json Web Token received is invalid");
     return false;
   }
-}
+};
 
 // Authentication middleware
-exports.auth = function (req, res, next) {
-  console.log('Middleware: jwt.auth');
-  if (!req.headers['authorization']) {
-    console.log('Error: Empty auth header');
+exports.auth = function(req, res, next) {
+  console.log("Middleware: jwt.auth");
+  if (!req.headers["authorization"]) {
+    console.log("Error: Empty auth header");
     res.sendStatus(401);
     return;
   }
 
-  let username = exports.verifyJWT(req.headers['authorization'].split(' ')[1], 'nodejs');
+  let username = exports.verifyJWT(
+    req.headers["authorization"].split(" ")[1],
+    "nodejs"
+  );
   if (!username) {
-    console.log('Error: JWT Verification failed');
+    console.log("Error: JWT Verification failed");
     res.sendStatus(401);
     return;
   }
-  
-  const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'notekeeper',
-    password: 'postgres',
-    port: 5432,
-  });
-  client.connect();
-  client.query('SELECT id FROM user_acc WHERE usrn = $1', [username], function (err, queryRes) {
-    res.locals.username = username;
-    res.locals.user_id = queryRes.rows[0].id;
-    next();
-  });
-}
+
+  let client = null;
+  if (process.env.DATABASE_URL) {
+    client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
+    });
+  } else {
+    client = new Client({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT
+    });
+  }
+  client
+    .connect()
+    .then(() => {
+      client.query(
+        "SELECT id FROM user_acc WHERE usrn = $1",
+        [username],
+        function(err, queryRes) {
+          if (err) {
+            next(err);
+          }
+          res.locals.username = username;
+          res.locals.user_id = queryRes.rows[0].id;
+          next();
+        }
+      );
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(512);
+    });
+};
