1ce90d1384e9a44c6d241d31669d98dc2be08a22
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-03-13 14:13:28 -0300

    Show/hide elements in the main page if you're logged or not

--- public/script/index.js
+++ public/script/index.js
@@ -3,6 +3,18 @@ $(function() {
     console.log(localStorage.getItem('token'));
 
     if (localStorage.getItem('token')) {
+        $('#create').show()
+        $('#read').show()
+        $('#signout').show()
+        $('#login').hide()
+        $('#signup').hide()
         $("#isLogged").append("Logged");
+    }
+    else {
+        $('#create').hide()
+        $('#read').hide()
+        $('#signout').hide()
+        $('#login').show()
+        $('#signup').show()
     }
 });
